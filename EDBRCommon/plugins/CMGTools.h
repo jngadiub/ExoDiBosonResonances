8644b6f96aa2338344841aa0b60de59bd1737426
tomei <>
2012-10-31 09:59:28 +0000

    Temporary commit

--- EDBRCommon/plugins/CMGTools.h
+++ EDBRCommon/plugins/CMGTools.h
@@ -1,24 +1,23 @@
-#ifndef __HiggsAna_HLLJJCommon_CMGTools_h__
-#define __HiggsAna_HLLJJCommon_CMGTools_h__
+#ifndef __ExoDiBosonResonances_EDBRCommon_CMGTools_h__
+#define __ExoDiBosonResonances_EDBRCommon_CMGTools_h__
 
-
-#include "HiggsAna/HLLJJCommon/interface/CompoundFactories.h"
-#include "HiggsAna/HLLJJCommon/interface/PFJetSmearFactory.h"
+#include "ExoDiBosonResonances/EDBRCommon/interface/CompoundFactories.h"
+//#include "ExoDiBosonResonances/EDBRCommon/interface/PFJetSmearFactory.h"
 #include "CMGTools/Common/plugins/PhysicsObjectProducer.h"
 
-typedef PhysicsObjectProducer<cmg::PFJetSmearFactory> PFJetSmearPOProducer;
+//typedef PhysicsObjectProducer<cmg::PFJetSmearFactory> PFJetSmearPOProducer;
 
 typedef PhysicsObjectProducer<cmg::DiGenParticleFactory> DiGenParticlePOProducer;
-typedef PhysicsObjectProducer<cmg::DiPFJetKinFitFactory> DiPFJetKinFitPOProducer;
-typedef PhysicsObjectProducer<cmg::DiJetKinFitFactory> DiJetKinFitPOProducer;
+//typedef PhysicsObjectProducer<cmg::DiPFJetKinFitFactory> DiPFJetKinFitPOProducer;
+//typedef PhysicsObjectProducer<cmg::DiJetKinFitFactory> DiJetKinFitPOProducer;
 
 typedef PhysicsObjectProducer<cmg::DiMuonDiJetFactory> DiMuonDiJetPOProducer;
 typedef PhysicsObjectProducer<cmg::DiElectronDiJetFactory> DiElectronDiJetPOProducer;
 typedef PhysicsObjectProducer<cmg::DiGenParticleDiGenParticleFactory> DiGenParticleDiGenParticlePOProducer;
 
-typedef PhysicsObjectProducer<cmg::DiMuonDiJetHiggsFactory> DiMuonDiJetHiggsPOProducer;
-typedef PhysicsObjectProducer<cmg::DiElectronDiJetHiggsFactory> DiElectronDiJetHiggsPOProducer;
-typedef PhysicsObjectProducer<cmg::DiGenParticleDiGenParticleHiggsFactory> DiGenParticleDiGenParticleHiggsPOProducer;
+//typedef PhysicsObjectProducer<cmg::DiMuonDiJetHiggsFactory> DiMuonDiJetHiggsPOProducer;
+//typedef PhysicsObjectProducer<cmg::DiElectronDiJetHiggsFactory> DiElectronDiJetHiggsPOProducer;
+//typedef PhysicsObjectProducer<cmg::DiGenParticleDiGenParticleHiggsFactory> DiGenParticleDiGenParticleHiggsPOProducer;
 
 
 #endif
