659e42e855cd620fd0a96eb85515cedd5542a046
bonato <>
2012-11-29 10:54:47 +0000

    Update dictionaries with EDBR classes

--- AnalysisDataFormats/src/classes.h
+++ AnalysisDataFormats/src/classes.h
@@ -27,6 +27,26 @@ namespace {
     edm::Wrapper<cmg::DiGenParticleDiGenParticle> edzgg;
     edm::Wrapper<std::vector<cmg::DiGenParticleDiGenParticle> > edzggv;
 
+
+
+    cmg::DiMuonDiJetEDBR dzh_;
+    std::vector<cmg::DiMuonDiJetEDBR> dzvh;
+    edm::Wrapper<cmg::DiMuonDiJetEDBR> edzh;
+    edm::Wrapper<std::vector<cmg::DiMuonDiJetEDBR> > edzvh;
+
+    cmg::DiElectronDiJetEDBR dzeh_;
+    std::vector<cmg::DiElectronDiJetEDBR> dzevh;
+    edm::Wrapper<cmg::DiElectronDiJetEDBR> edezh;
+    edm::Wrapper<std::vector<cmg::DiElectronDiJetEDBR> > edzevh;
+
+    cmg::DiGenParticleDiGenParticleEDBR dzggh_;
+    std::vector<cmg::DiGenParticleDiGenParticleEDBR> dzggvh;
+    edm::Wrapper<cmg::DiGenParticleDiGenParticleEDBR> edzggh;
+    edm::Wrapper<std::vector<cmg::DiGenParticleDiGenParticleEDBR> > edzggvh;
+
+
+
+
     cmg::GenParticlePtr gpptr_;
 
     edm::Ptr<cmg::DiObject<cmg::BaseJet,cmg::BaseJet> > vbtfptr_;
