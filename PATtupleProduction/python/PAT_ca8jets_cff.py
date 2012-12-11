1b6e42bd0e88320f136927bf34f230d8d8242293
hinzmann <>
2012-12-11 11:07:52 +0000

    calculate ca8 jets on the fly

--- PATtupleProduction/python/PAT_ca8jets_cff.py
+++ PATtupleProduction/python/PAT_ca8jets_cff.py
@@ -19,7 +19,7 @@ patJetCorrFactorsCA8CHS = patJetCorrFactors.clone()
 patJetCorrFactorsCA8CHS.src = jetSource
 # will need to add L2L3 corrections in the cfg
 patJetCorrFactorsCA8CHS.levels = ['L1FastJet', 'L2Relative', 'L3Absolute']
-patJetCorrFactorsCA8CHS.payload = 'AK7PFchs'
+patJetCorrFactorsCA8CHS.payload = 'CA8PFchs'
 patJetCorrFactorsCA8CHS.useRho = True
 
 # parton and gen jet matching
@@ -76,3 +76,98 @@ PATCMGJetSequenceCA8CHS = cms.Sequence(
     patJetsCA8CHS +
     selectedPatJetsCA8CHS
     )
+
+# JETS PRUNED CA8 ----------------------------
+
+from RecoJets.JetProducers.ak5PFJetsPruned_cfi import ak5PFJetsPruned
+ca8PFJetsCHSpruned = ak5PFJetsPruned.clone(
+    src = 'pfNoPileUp',
+    jetPtMin = cms.double(10.0),
+    doAreaFastjet = cms.bool(True),
+    rParam = cms.double(0.8),
+    jetAlgorithm = cms.string("CambridgeAachen"),
+    )
+
+jetSource = 'ca8PFJetsCHSpruned'
+
+# corrections 
+from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
+patJetCorrFactorsCA8CHSpruned = patJetCorrFactors.clone()
+patJetCorrFactorsCA8CHSpruned.src = jetSource
+# will need to add L2L3 corrections in the cfg
+patJetCorrFactorsCA8CHSpruned.levels = ['L1FastJet', 'L2Relative', 'L3Absolute']
+patJetCorrFactorsCA8CHSpruned.payload = 'CA8PFchs'
+patJetCorrFactorsCA8CHSpruned.useRho = True
+
+# parton and gen jet matching
+
+from PhysicsTools.PatAlgos.mcMatchLayer0.jetMatch_cfi import *
+patJetPartonMatchCA8CHSpruned = patJetPartonMatch.clone()
+patJetPartonMatchCA8CHSpruned.src = jetSource
+patJetGenJetMatchCA8CHSpruned = patJetGenJetMatch.clone()
+patJetGenJetMatchCA8CHSpruned.src = jetSource
+patJetGenJetMatchCA8CHSpruned.matched = 'ca8GenJetsNoNu'
+
+from PhysicsTools.PatAlgos.mcMatchLayer0.jetFlavourId_cff import *
+patJetPartonAssociationCA8CHSpruned = patJetPartonAssociation.clone()
+patJetPartonAssociationCA8CHSpruned.jets = jetSource
+
+# pat jets
+
+from PhysicsTools.PatAlgos.producersLayer1.jetProducer_cfi import *
+patJetsCA8CHSpruned = patJets.clone()
+patJetsCA8CHSpruned.jetSource = jetSource
+patJetsCA8CHSpruned.addJetCharge = False
+patJetsCA8CHSpruned.embedCaloTowers = False
+patJetsCA8CHSpruned.embedPFCandidates = False
+patJetsCA8CHSpruned.addAssociatedTracks = False
+patJetsCA8CHSpruned.addBTagInfo = False
+patJetsCA8CHSpruned.addDiscriminators = False
+patJetsCA8CHSpruned.getJetMCFlavour = False
+patJetsCA8CHSpruned.jetCorrFactorsSource = cms.VInputTag(cms.InputTag('patJetCorrFactorsCA8CHSpruned'))
+patJetsCA8CHSpruned.genPartonMatch = cms.InputTag('patJetPartonMatchCA8CHSpruned')
+patJetsCA8CHSpruned.genJetMatch = cms.InputTag('patJetGenJetMatchCA8CHSpruned')
+
+from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
+selectedPatJetsCA8CHSpruned = selectedPatJets.clone()
+selectedPatJetsCA8CHSpruned.src = 'patJetsCA8CHSpruned'
+selectedPatJetsCA8CHSpruned.cut = 'pt()>20'
+
+jetMCSequenceCA8CHSpruned = cms.Sequence(
+    patJetPartonMatchCA8CHSpruned +
+    patJetGenJetMatchCA8CHSpruned
+    )
+
+PATCMGJetSequenceCA8CHSpruned = cms.Sequence(
+    ca8PFJetsCHSpruned +
+    jetMCSequenceCA8CHSpruned +
+    patJetCorrFactorsCA8CHSpruned +
+    patJetsCA8CHSpruned +
+    selectedPatJetsCA8CHSpruned
+    )
+
+
+#### Adding Nsubjetiness
+
+selectedPatJetsCA8CHSwithNsub = cms.EDProducer("NjettinessAdder",
+                              src=cms.InputTag("selectedPatJetsCA8CHS"),
+                              cone=cms.double(0.7)
+                              )
+
+#### Adding QJets
+
+selectedPatJetsCA8CHSwithQjets = cms.EDProducer("QjetsAdder",
+                               src=cms.InputTag("selectedPatJetsCA8CHSwithNsub"),
+                               zcut=cms.double(0.1),
+                               dcutfctr=cms.double(0.5),
+                               expmin=cms.double(0.0),
+                               expmax=cms.double(0.0),
+                               rigidity=cms.double(0.1),
+                               ntrial = cms.int32(50),
+                               cutoff=cms.double(100.0),
+                               jetRad= cms.double(0.7),
+                               jetAlgo=cms.string("AK"),
+                               preclustering = cms.int32(50),
+                              )
+
+ca8Jets = cms.Sequence( jetMCSequenceCA8CHS + PATCMGJetSequenceCA8CHS + jetMCSequenceCA8CHSpruned + PATCMGJetSequenceCA8CHSpruned + selectedPatJetsCA8CHSwithNsub + selectedPatJetsCA8CHSwithQjets )
