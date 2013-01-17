9a76a4df7907835cee9e981a4d8941ddbfa6bc95
tomei <>
2013-01-17 18:01:23 +0000

    Fixed a mistake in Run2012C PromptReco JSON

--- EDBRCommon/data/makePUHistsData.sh
+++ EDBRCommon/data/makePUHistsData.sh
@@ -9,7 +9,8 @@ BaseDir=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV
 AB13JulJSON=${BaseDir}/Reprocessing/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt
 ArecoverJSON=${BaseDir}/Reprocessing/Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt
 C24AugJSON=${BaseDir}/Reprocessing/Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt
-CDPromptJSON=${BaseDir}/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt
+CPromptJSON=${BaseDir}/Prompt/Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt
+DPromptJSON=${BaseDir}/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt
 
 # Some printouts
 echo "Mixing the following JSON files:"
@@ -17,13 +18,15 @@ echo "A rereco: $AB13JulJSON"
 echo "A recover rereco: $ArecoverJSON"
 echo "B rereco: $AB13JulJSON"
 echo "C rereco: $C24AugJSON"
-echo "C prompt: $CDPromptJSON"
-echo "D prompt: $CDPromptJSON"
+echo "C prompt: $CPromptJSON"
+echo "D prompt: $DPromptJSON"
 
 mergeJSON.py ${AB13JulJSON} ${ArecoverJSON} --output=fullABJSON.txt
 mergeJSON.py fullABJSON.txt ${C24AugJSON} --output=fullReRecoJSON.txt
-filterJSON.py --min 198523 --max 999999 ${CDPromptJSON} --output promptJSON.txt
-mergeJSON.py fullReRecoJSON.txt promptJSON.txt --output=goldenAnalysisJSON.txt
+filterJSON.py --min 198934 --max 203746 ${CPromptJSON} --output myCpromptJSON.txt
+filterJSON.py --min 203768 --max 999999 ${DPromptJSON} --output myDpromptJSON.txt
+mergeJSON.py myCpromptJSON.txt myDpromptJSON.txt --output=fullPromptJSON.txt
+mergeJSON.py fullReRecoJSON.txt fullPromptJSON.txt --output=goldenAnalysisJSON.txt
 
 # Golden JSON
 #AnalysisJSON=${BaseDir}/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt
