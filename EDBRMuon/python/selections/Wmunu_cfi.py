2596e2f3915bb4e5fb4d6bfe2c69aa62abc14391
tomei <>
2013-01-31 17:50:10 +0000

    Fixing mistake

--- EDBRMuon/python/selections/Wmunu_cfi.py
+++ EDBRMuon/python/selections/Wmunu_cfi.py
@@ -7,7 +7,7 @@ Wmunu_kinematics = cms.PSet(
 
 Wmunu_quality = cms.PSet(
     isGB=cms.string('leg1().getSelection(\"cuts_HPTGBmuon\")'),
-    isIsolated=cms.string('leg1.sourcePtr.trackIso)/leg1.pt) < 0.1')
+    isIsolated=cms.string('leg1.sourcePtr.trackIso/leg1.pt < 0.1')
     )
 Wmunu_validation  = cms.PSet(
     ptmatch = cms.string('(leg1().pt()-leg2().getleppt())/leg1().pt()<0.01&&(leg1().pt()-leg2().getleppt())/leg1().pt()>-0.01'),
