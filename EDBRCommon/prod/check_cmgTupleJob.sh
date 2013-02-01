3cb314b7bfaa985585c33b686633e6b6b9198b8c
bonato <>
2013-02-01 16:56:56 +0000

    Improvements to the script for sanity check of cmgTuple jobs

--- EDBRCommon/prod/check_cmgTupleJob.sh
+++ EDBRCommon/prod/check_cmgTupleJob.sh
@@ -6,9 +6,17 @@ OUTPATH=""
 LOGDIR="./logs/"
 if [ $# -lt 1 ]
 then
-    echo need one input, a string that is either \"DATA\" or \"MC\"
+    echo need at least one input, a string that is either \"DATA\" or \"MC\". 
+    echo Second \(optional\) input is an integer that if it is \>0 resubmits the failed jobs.
     exit
 fi
+
+RESUB=0
+if [ $# -ge 2 ]
+    then
+    RESUB=$2
+fi
+
 
 if [ $1 == "DATA" ]
     then
@@ -34,29 +42,51 @@ cd $LOGDIR
 for sample in "${SAMPLE_ARRAY1[@]}"
 do
   cd ./$sample
+  if [ $? -ne 0 ]
+      then
+      echo "Directory $sample does not exist"
+      continue
+  fi
+
   echo I am in $(pwd):
 
 
   for job in $(/bin/ls -d Job*)
     do
     cd ./$job
+
+    STATUS=0
     egrep exception -ir LSF*/* &> /dev/null
-    if [ $? == 0 ]
+    if [ $? -eq 0 ]
+	then 
+	let STATUS=STATUS+1
+    fi
+
+    egrep segmentation -ir LSF*/* &> /dev/null
+    if [ $? -eq 0 ]
+	then 
+	let STATUS=STATUS+1
+    fi
+ 
+   if [ $STATUS -gt 0 ]
 	then
 	echo 
 	echo "+++"
 	echo EXception in ./$sample/$job
 	EXT=${job#"Job_"}
-	echo Removing ${OUTPATH}/${sample}/cmgTuple_${EXT}.root 
-#cmsRm ${OUTPATH}/${sample}/cmgTuple_${EXT}.root 
-#echo Submitting $job
-	#bsub -q cmscaf1nd -J "${sample}_${job}" batchScript.sh
+	if [ $RESUB -gt 0 ]
+	    then
+	    echo Removing ${OUTPATH}/${sample}/cmgTuple_${EXT}.root  
+	    cmsRm ${OUTPATH}/${sample}/cmgTuple_${EXT}.root 
+	    echo Resubmitting $job
+	    bsub -q 8nh -J "${sample}_${job}" batchScript.sh
+	    #rename dir with log for book-keeping
+	    for lsfdir in $(/bin/ls -d LSF* )
+	      do
+	      mv $lsfdir old${lsfdir}
+	    done
+	fi
 
-
-       #  for lsfdir in $(/bin/ls -d LSF* )
-	#  do
-	  #  mv $lsfdir old${lsfdir}
-	 # done
 	echo "---"
     fi
     cd ../ &> /dev/null
