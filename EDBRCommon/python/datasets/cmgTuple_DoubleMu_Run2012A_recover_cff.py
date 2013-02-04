c9432aca46c57f7d3ecffa00c354abc7e448dfec
bonato <>
2013-02-04 13:41:02 +0000

    add cmgTuple for DoubleMu from productionv1

---
+++ EDBRCommon/python/datasets/cmgTuple_DoubleMu_Run2012A_recover_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+cmgFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",
+                                                noEventSort = cms.untracked.bool(True),
+                                                duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                                                fileNames = cmgFiles
+                                                )
+
+cmgFiles.extend([
+       '/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/DoubleMu_Run2012A_recover/cmgTuple_0.root',
+       '/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/DoubleMu_Run2012A_recover/cmgTuple_1.root'
+    ])
