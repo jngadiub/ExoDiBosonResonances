e920c5cac6288f63ff080157f93e5081ac4d6996
santanas <>
2013-02-06 09:15:51 +0000

    included all samples in the list

--- EDBRCommon/prod/check_cmgTupleJob.sh
+++ EDBRCommon/prod/check_cmgTupleJob.sh
@@ -22,8 +22,9 @@ if [ $1 == "DATA" ]
     then
     echo "Checking data"
 #    SAMPLE_ARRAY1=(DoubleMu_Run2012A_13Jul2012 DoubleMu_Run2012A_recover DoubleMu_Run2012B_13Jul2012 DoubleMu_Run2012C_24Aug2012 DoubleMu_Run2012D_PRv1 DoublePhotonHighPt_Run2012B_13Jul2012 DoublePhotonHighPt_Run2012C_24Aug2012 DoublePhotonHighPt_Run2012C_PRv2 DoublePhotonHighPt_Run2012D_PRv1 Photon_Run2012A_13Jul2012 Photon_Run2012A_recover )  #ZjetsMadgraph WZPythia ZZPythia )
-    SAMPLE_ARRAY1=(DoubleMu_Run2012C_PRv2)
-    OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/"
+    SAMPLE_ARRAY1=( DoubleMu_Run2012A_13Jul2012 DoubleMu_Run2012A_recover DoubleMu_Run2012B_13Jul2012 DoubleMu_Run2012C_24Aug2012 DoubleMu_Run2012C_PRv2 DoubleMu_Run2012D_PRv1) 
+    #OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/"
+    OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/santanas_TEST__05_02_2013/Run2012/presel/" 
     LOGDIR="./logs/data12/"
 fi
 
@@ -31,7 +32,8 @@ if [ $1 == "MC" ]
     then
     echo "Checking MC"
     SAMPLE_ARRAY1=(DYJetsPt50To70 DYJetsPt70To100 DYJetsPt100 TTBAR WZ ZZ)
-    OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/"
+    #OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/productionV1/Run2012/presel/"
+    OUTPATH="/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/santanas_TEST__05_02_2013/Summer12/presel/"
     LOGDIR="./logs/Summer12/"
 fi
 
