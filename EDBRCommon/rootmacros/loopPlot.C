d9a939cc942099061565b5c0c3697a4f0cec394e
tomei <>
2013-02-14 09:12:43 +0000

    Centralizing flags

--- EDBRCommon/rootmacros/loopPlot.C
+++ EDBRCommon/rootmacros/loopPlot.C
@@ -14,22 +14,35 @@ gErrorIgnoreLevel=kFatal;//suppresses all info messages
 
 void loopPlot(){
 
-  setTDRStyle();
+  gROOT->ProcessLine(".L makeHisto.C+");
+  gROOT->ProcessLine(".L plotHisto.C+");
   
-  int statusMakeHisto = gROOT->ProcessLine(".L makeHisto.C+");
-  int statusPlotHisto = gROOT->ProcessLine(".L plotHisto.C+");
-
-  if((statusMakeHisto != 0) || (statusPlotHisto != 0)) {
-    printf("Macro compilation failed!!! Aborting...\n");
-    return;
-  }
+  setTDRStyle();
+ 
+  /// Boolean flags to steer the histogram making
+  bool wantElectrons = true; // Will make histograms for electrons
+  bool wantMuons     = false; // Will make histograms for muons
+  bool wantSideband  = true; // Will make histograms for sideband region
+  bool wantSignal    = false; // Will make histograms for signal region
+  int  wantNXJets    = 1; // Will make histograms for 1 or 2 jet topology
+  int flavour = 0; 
+  if(wantElectrons) flavour=11;
+  if(wantMuons) flavour=13;
+  
+  /// Luminosity value in pb^-1
+  double lumiValue = 19477.6;
+  /// k-factor for LO to NNLO
+  double kFactor = 1.2;
+  /// Should we scale the histograms to data?
+  bool scaleToData = true;
   
   /// Path to wherever the files with the trees are. 
   //std::string pathToTrees="/afs/cern.ch/user/b/bonato/work/PhysAnalysis/EXOVV_2012/analyzer_trees/productionv1/";
-  std::string pathToTrees="/afs/cern.ch/user/t/tomei/EXOVV_2012/analyzer_trees/productionTEST/";
+  //std::string pathToTrees="/afs/cern.ch/user/t/tomei/EXOVV_2012/analyzer_trees/productionTEST/";
+  std::string pathToTrees="/afs/cern.ch/user/b/bonato/work/PhysAnalysis/EXOVV_2012/analyzer_trees/productionv1/preselCA8/";
   /// Path to wherever you want to put the histograms (figures) in.
   //std::string outputDir = pathToTrees+"./test_outPlots";
-  std::string outputDir = "./doubleJetElectrons";
+  std::string outputDir = "./singleJetElectrons";
 
   /// Setup names of data files for trees.
   const int nDATA=6;//set to zero if you don't want to plot
@@ -71,12 +84,7 @@ void loopPlot(){
   std::vector<std::string> fHistosData;
   std::vector<std::string> fHistosMC;
  
-
-  /// Luminosity value in pb^-1
-  double lumiValue = 19600.0*0.2;
-  /// k-factor for LO to NNLO
-  double kFactor = 1.2;
-
+  char buffer[256];
   printf("All strings set\n");
 
   /// ----------------------------------------------------------------
@@ -95,17 +103,16 @@ void loopPlot(){
   printf("\nStart making histograms\n\n");
 
   //loop over data files and make histograms individually for each of them
-  char buffer[256];
   for(int i=0;i<nDATA;i++){
     TFile *fileData = TFile::Open(fData.at(i).c_str());
     TTree *treeData = (TTree*)fileData->Get("SelectedCandidates");
     std::cout<<"\n-------\nRunning over "<<dataLabels[i].c_str()<<std::endl;
     EDBRHistoMaker* maker = new EDBRHistoMaker(treeData, 
-					       true, //wantElectrons
-					       false, //wantMuons
-					       true, //wantSideband
-					       true, //wantSignal
-					       2);//wantNXJets
+					       wantElectrons,
+					       wantMuons,
+					       wantSideband,
+					       wantSignal,
+					       wantNXJets);
     sprintf(buffer,"histos_%s.root",dataLabels[i].c_str());
     maker->setUnitaryWeights(true);
     maker->Loop(buffer);
@@ -123,11 +130,11 @@ void loopPlot(){
     TTree *treeMC = (TTree*)fileMC->Get("SelectedCandidates");
     std::cout<<"\n-------\nRunning over "<<mcLabels[i].c_str()<<std::endl;
     EDBRHistoMaker* maker = new EDBRHistoMaker(treeMC, 
-					       true, 
-					       false, 
-					       true, 
-					       true, 
-					       2);
+					       wantElectrons, 
+					       wantMuons, 
+					       wantSideband, 
+					       wantSignal, 
+					       wantNXJets);
     sprintf(buffer,"histos_%s.root",mcLabels[i].c_str());
     maker->setUnitaryWeights(false);
     maker->Loop(buffer);
@@ -179,7 +186,9 @@ void loopPlot(){
 						 fHistosMC,
 						 lumiValue,
 						 kFactor,
-						 true);//bool scaleToData
+						 wantNXJets,
+						 flavour,
+						 scaleToData);
   std::cout<<"Set output dir"<<std::endl;
   plotter->setOutDir(outputDir);
   plotter->setDebug(true);
