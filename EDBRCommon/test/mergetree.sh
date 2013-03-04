7a359d9bb8ffee955b6fbda3665aa1428e6913e1
shuai <>
2013-03-04 15:55:49 +0000

    *** empty log message ***

--- EDBRCommon/test/mergetree.sh
+++ EDBRCommon/test/mergetree.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SIGNAL=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/control/fullsig
-SIDEBAND=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/control/fullsideband
-OUTDIR=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/control/merged
+SIGNAL=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/fullsig
+SIDEBAND=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/fullsideband
+OUTDIR=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/merged
 
 mkdir -p $OUTDIR
 
 for file in $(ls $SIGNAL | grep root)
 do
-	echo $line
+	#echo $file
 	root -l -q mergetree.C\(\"${SIGNAL}/${file}\",\"${SIDEBAND}/${file}\",\"${OUTDIR}/${file}\"\)
 done
