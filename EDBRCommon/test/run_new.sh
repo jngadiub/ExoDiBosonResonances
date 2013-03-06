a791897c2c358e064de5ef1bffd9fd7bd3005eca
shuai <>
2013-03-06 12:41:25 +0000

    new script to submit trees

---
+++ EDBRCommon/test/run_new.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+MYWORKAREA=$CMSSW_BASE/src/
+cd $MYWORKAREA
+eval `scram runtime -sh`
+
+LOGDIR=$1
+cd $LOGDIR
+
+cmsRun analyzerEDBR_<type>_<sample>_cfg.py  &> log_analyzerEDBR_<type>_<sample>.out
+
+cp treeEDBR_<sample>.root ../../
+
