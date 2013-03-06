48030e75a812f9ac413c05dff6465c2d06d98994
shuai <>
2013-03-06 19:32:38 +0000

    corrected

--- EDBRCommon/test/mergetree.C
+++ EDBRCommon/test/mergetree.C
@@ -5,37 +5,48 @@
 #include <iostream>
 
 
-void mergetree(TString file1 = "/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/control/fullsig/treeEDBR_RSG_WW_lvjj_c0p2_M600_xww.root", TString file2 = "/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv5/control/fullsideband/treeEDBR_RSG_WW_lvjj_c0p2_M600_xww.root",TString outfile = "mergetreeTest.root")
+void mergetree(TString file1 = "/afs/cern.ch/work/s/shuai/public/diboson/trees/test/testnewsh/fullsig/treeEDBR_TTBAR_xww.root", TString file2 = "/afs/cern.ch/work/s/shuai/public/diboson/trees/test/testnewsh/fullsideband/treeEDBR_TTBAR_xww.root",TString outfile = "mergetreeTest.root")
 {
 	cout<<"file1 :"<<file1<<endl;
 	cout<<"file2 :"<<file2<<endl;
 	TChain * chain = new TChain("SelectedCandidates");
 	chain->Add(file1);
-	cout<<"file1 entries: "<<chain->GetEntries()<<endl;
 	int nsig = chain->GetEntries();
 	chain->Add(file2);
-	cout<<"file1+file2 entries: "<<chain->GetEntries()<<endl;
 	int ntotal = chain->GetEntries();
+	int nside = ntotal-nsig;
+	cout<<"file1 entries: "<<nsig<<endl;
+	cout<<"file2 entries: "<<nside<<endl;
+	cout<<"file1+file2 entries: "<<ntotal<<endl;	
 	
     TFile * outputfile = new TFile(outfile,"RECREATE");
 	TTree * outTree = chain->CloneTree(0);
 
     int nCands;
+	//int nEvt;
     chain->SetBranchAddress("nCands",&nCands);
+    //chain->SetBranchAddress("nEvt",&nEvt);
 
 	//save events with signal region candidate for filesig
 	for(int i =0; i<nsig; i++)
 	{
+		if(i%100==0)cout<<i<<endl;
+		//if(i>1000)break;
 		chain->GetEntry(i);
-		if(nCands>0)outTree->Fill();//nCands could be 1 or 2 ( 1 signal region candidate with/without sideband candidate  )
+		//cout<<nEvt<<endl;
+		//chain->GetEntry(nsig+i);
+		//cout<<nEvt<<endl;
+		
+		if(nCands>0)outTree->Fill();//this event have signal region candidate ( 2 means there are Wmunu and Welenu. This will be filtered by loose lepton veto  )
+		else
+		{
+			int j = nsig+i;//corresponding sideband event
+			chain->GetEntry(j);
+			if(nCands>0)outTree->Fill();
+		}
+		
 	}
 
-	//save events in sideband region, not overlaped with signal region
-	for(int i =nsig; i<ntotal; i++)
-	{
-		chain->GetEntry(i);
-		if(nCands==1)outTree->Fill();//have only one sideband candidate, to avoid overlap
-	}
 
 	cout<<"outTree entries: "<<outTree->GetEntries()<<endl;
 	
