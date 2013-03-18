b08181260b731500910a9b35ec851bf50534bff4
tomei <>
2013-03-18 09:54:38 +0000

    Adding more signal samples

---
+++ EDBRCommon/python/datasets/cmgTuple_BulkG_ZZ_lljj_c0p2_M1400_cff.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ('PoolSource',fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend([
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M1400/cmgTuple_0.root',
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M1400/cmgTuple_1.root',
+]);
---
+++ EDBRCommon/python/datasets/cmgTuple_BulkG_ZZ_lljj_c0p2_M600_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ('PoolSource',fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend([
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M600/cmgTuple_0.root',
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M600/cmgTuple_1.root',
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M600/cmgTuple_2.root',
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M600/cmgTuple_3.root',
+'/store/group/phys_exotica/leptonsPlusJets/ExoDiBosonResonances/CMGtuple/tomei_edbr_vv_20130313/Summer12/CA8//BulkG_ZZ_lljj_c0p2_M600/cmgTuple_4.root',
+]);
