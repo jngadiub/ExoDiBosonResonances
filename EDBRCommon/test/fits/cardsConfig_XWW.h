b052b09c363da90236ffe0090a8179b10e21f540
shuai <>
2013-04-25 19:18:28 +0000

    olf configure

--- EDBRCommon/test/fits/cardsConfig_XWW.h
+++
@@ -1,8 +0,0 @@
-const std::string wsDir="FitSidebandsMJJ_WW_20130423/";
-const std::string datacardDir("DataCards_XWW_20130423/");
-/////float mZZmin_ = 600;  // this should be synchronized with startFit in fitBackground.cpp
-float mZZmax_=3000;
-const int jetCats =1;  // 1 for only 1jet case and 2 for both
-bool isZZChannel=false;
-bool unblind=false;
-const string lepton_Label="ALL";//"ALL" //"MU" //"ELE"
--- EDBRCommon/test/fits/cardsConfig_XZZ.h
+++
@@ -1,8 +0,0 @@
-const std::string wsDir="FitSidebandsMJJ_ZZ_20130424/";
-const std::string datacardDir("DataCards_XZZ_20130425/");
-/////float mZZmin_ = 600;  // this should be synchronized with startFit in fitBackground.cpp
-float mZZmax_=2600;
-const int jetCats =2;  // 1 for only 1jet case and 2 for both
-bool isZZChannel=true;
-bool unblind=true;
-const string lepton_Label="ALL";//"ALL" //"MU" //"ELE"
