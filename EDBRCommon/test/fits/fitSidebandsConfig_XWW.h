d6303739634c4631dc9e5fccfb2d016f62706af2
shuai <>
2013-04-25 14:15:17 +0000

    update

--- EDBRCommon/test/fits/fitSidebandsConfig_XWW.h
+++ EDBRCommon/test/fits/fitSidebandsConfig_XWW.h
@@ -1,12 +1,12 @@
 
-const std::string myOutDir="FitSidebandsMJJ_CA8_WW_V15_AB/";
-const string inDirSIG="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv7_newMJ/AnaSigTree_from50_noConv/";
-const string inDirSB ="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv7_newMJ/AnaSBTree_from50_noConv/";
+const std::string myOutDir="FitSidebandsMJJ_CA8_WW_V16_AB/";
+const string inDirSIG="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv7_newMJ/AnaSigTree_from40_noConv/";
+const string inDirSB ="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv7_newMJ/AnaSBTree_from40_noConv/";
 
-unsigned int jetCats = 2;//1 for only 1 jet case, 2 for both
-bool isZZChannel=true;//this will change only the file list
-string leptStr="ALL";//"MU" //"ELE"//"ALL"
-const std::string InTreeName="SelectedCandidates";
+unsigned int jetCats = 1;//1 for only 1 jet case, 2 for both
+bool isZZChannel=false;//this will change only the file list
+string leptStr="MU";//"MU" //"ELE"//"ALL"
+const std::string InTreeName="SelectedCandidatesAB";
 const bool useAlphaVV=false;
 const unsigned int nToys = 500;
 
