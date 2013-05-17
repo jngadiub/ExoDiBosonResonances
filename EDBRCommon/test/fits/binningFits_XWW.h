abc654423373aa77fde452a0cf183f05b1cbbd44
shuai <>
2013-05-17 10:12:11 +0000

    fit range 800-2600

--- EDBRCommon/test/fits/binningFits_XWW.h
+++ EDBRCommon/test/fits/binningFits_XWW.h
@@ -2,12 +2,12 @@
 #define NBINS_H 
 
 const int nBins1=20;
-const double bins1[nBins1]={520,560,600,640,680,720,760,800,840,920,1000,1100,1250,1400,1600,1800,2000,2200,2400,2600};
+const double bins1[nBins1]={520,560,600,640,680,720,760,800,840,920,1000,1100,1250,1400,1600,1800,2000,2200,2400,2800};
 //binning for double Jet topology 
 const int nBins2=16;
 const double bins2[nBins2]={480,500,520,560,600,640,680,720,760,800,840,920,
 			    1000,1100,1250,1400};
 
-const double startFit = 1000.0;
+const double startFit = 800.0;
 
 #endif
