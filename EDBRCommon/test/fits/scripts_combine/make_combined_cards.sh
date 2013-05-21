40e5021bfa5947eedc95d2cb42f0b37d546e43be
mmozer <>
2013-05-21 12:02:19 +0000

    support different process labels

--- EDBRCommon/test/fits/scripts_combine/make_combined_cards.sh
+++ EDBRCommon/test/fits/scripts_combine/make_combined_cards.sh
@@ -6,31 +6,33 @@ WHAT=$2
 
 COMB=""
 
+label="xzz"
+
 #if expr index $MASS .5 > /dev/null; then MASSD="$MASS"; else MASSD="$MASS.0"; fi
 if echo $M | grep -F -q .5; then MASSD="$MASS"; else MASSD="$MASS.0"; fi
 
-EXOZZ1JLP="xzz_ee1JLP=xzz_ee1JLP.${MASS}.txt xzz_mm1JLP=xzz_mm1JLP.${MASS}.txt"
-EXOZZ1JHP="xzz_ee1JHP=xzz_ee1JHP.${MASS}.txt xzz_mm1JHP=xzz_mm1JHP.${MASS}.txt"
-EXOZZ2J="xzz_ee2J=xzz_ee2J.${MASS}.txt xzz_mm2J=xzz_mm2J.${MASS}.txt"
-EXOZZ1JELE="xzz_ee1JLP=xzz_ee1JLP.${MASS}.txt xzz_ee1JHP=xzz_ee1JHP.${MASS}.txt"
-EXOZZ1JMU="xzz_mm1JLP=xzz_mm1JLP.${MASS}.txt xzz_mm1JHP=xzz_mm1JHP.${MASS}.txt"
+EXOZZ1JLP="${label}_ee1JLP=${label}_ee1JLP.${MASS}.txt ${label}_mm1JLP=${label}_mm1JLP.${MASS}.txt"
+EXOZZ1JHP="${label}_ee1JHP=${label}_ee1JHP.${MASS}.txt ${label}_mm1JHP=${label}_mm1JHP.${MASS}.txt"
+EXOZZ2J="${label}_ee2J=${label}_ee2J.${MASS}.txt ${label}_mm2J=${label}_mm2J.${MASS}.txt"
+EXOZZ1JELE="${label}_ee1JLP=${label}_ee1JLP.${MASS}.txt ${label}_ee1JHP=${label}_ee1JHP.${MASS}.txt"
+EXOZZ1JMU="${label}_mm1JLP=${label}_mm1JLP.${MASS}.txt ${label}_mm1JHP=${label}_mm1JHP.${MASS}.txt"
 EXOZZ1J="$EXOZZ1JLP $EXOZZ1JHP "
 EXOZZ="$EXOZZ1JLP $EXOZZ1JHP $EXOZZ2J "
 
 ## ZZ 2Q
 #if (( $MASS >= 100)); then 
 COMB="$COMB $EXOZZ";
-combineCards.py -S $EXOZZ1JLP > comb_xzz_2l1JLP.txt
-combineCards.py -S $EXOZZ1JHP > comb_xzz_2l1JHP.txt
-combineCards.py -S $EXOZZ1JELE > comb_xzz_ee1J.txt
-combineCards.py -S $EXOZZ1JMU > comb_xzz_mm1J.txt
+combineCards.py -S $EXOZZ1JLP > comb_${label}_2l1JLP.txt
+combineCards.py -S $EXOZZ1JHP > comb_${label}_2l1JHP.txt
+combineCards.py -S $EXOZZ1JELE > comb_${label}_ee1J.txt
+combineCards.py -S $EXOZZ1JMU > comb_${label}_mm1J.txt
 if (( $MASS < 800)); then 
-    combineCards.py -S $EXOZZ2J   > comb_xzz_2l2J.txt
-    combineCards.py -S $EXOZZ1J   > comb_xzz_2l1J.txt
-#    combineCards.py -S $EXOZZ     > comb_xzz.txt
-    combineCards.py -S $EXOZZ1J   > comb_xzz.txt
+    combineCards.py -S $EXOZZ2J   > comb_${label}_2l2J.txt
+    combineCards.py -S $EXOZZ1J   > comb_${label}_2l1J.txt
+#    combineCards.py -S $EXOZZ     > comb_${label}.txt
+    combineCards.py -S $EXOZZ1J   > comb_${label}.txt
 else
-    combineCards.py -S $EXOZZ1J   > comb_xzz.txt
+    combineCards.py -S $EXOZZ1J   > comb_${label}.txt
 fi;
 ###combineCards.py -S $COMB > comb.txt
 
