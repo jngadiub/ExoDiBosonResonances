3d76525b6f2bb9054e41240134a9cb527cd76ad3
shuai <>
2013-05-30 13:16:04 +0000

    W' weights

--- EDBRElectron/python/HLTWeights_Wele_cff.py
+++ EDBRElectron/python/HLTWeights_Wele_cff.py
@@ -10,37 +10,14 @@ dummyHLTEleEffWeights =  cms.PSet( lumi = cms.double(1.),#dummy
                                hlt  = cms.VPSet( cms.PSet(bin = cms.vdouble(0.0,999.0,0.0,999. ,1.0))),
                                )
 
-eleIdSF_Run2012= cms.VPSet( ### dummy at one for now
-    cms.PSet(bin = cms.vdouble(0.0,0.9,20.,40. ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.9,1.2,20.,40. ,1.000)),
-    cms.PSet(bin = cms.vdouble(1.2,2.1,20.,40. ,1.000)),
-    cms.PSet(bin = cms.vdouble(2.1,2.4,20.,40. ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.0,0.9,40.,999. ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.9,1.2,40.,999. ,1.000)),
-    cms.PSet(bin = cms.vdouble(1.2,2.1,40.,999. ,1.000)),
-    cms.PSet(bin = cms.vdouble(2.1,2.4,40.,999. ,1.000)),
+eleIdSF_Run2012= cms.VPSet( ### eta bin of lepton, pt bin of lepton, eff
+    cms.PSet(bin = cms.vdouble(0.0,   1.4442,   0.0,   9999.,   0.98)),
+    cms.PSet(bin = cms.vdouble(1.566,    2.5,   0.0,   9999.,   0.98)),
     )
 
-singleEleTrigSF_Run2012= cms.VPSet(
-    cms.PSet(bin = cms.vdouble(0.0,0.9, 0.0,0.9 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.0,0.9, 0.9,1.2 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.0,0.9, 1.2,2.1 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.0,0.9, 2.1,2.4 ,1.000)),
-
-    cms.PSet(bin = cms.vdouble(0.9, 1.2, 0.0,0.9 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.9, 1.2, 0.9,1.2 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.9, 1.2, 1.2,2.1 ,1.000)),
-    cms.PSet(bin = cms.vdouble(0.9, 1.2, 2.1,2.4 ,1.000)),
-
-    cms.PSet(bin = cms.vdouble(1.2, 2.1, 0.0,0.9 ,1.000)),
-    cms.PSet(bin = cms.vdouble(1.2, 2.1, 0.9,1.2 ,1.000)),
-    cms.PSet(bin = cms.vdouble(1.2, 2.1, 1.2,2.1 ,1.000)),
-    cms.PSet(bin = cms.vdouble(1.2, 2.1, 2.1,2.4 ,1.000)),
-
-    cms.PSet(bin = cms.vdouble(2.1, 2.4, 0.0,0.9 ,1.000)),
-    cms.PSet(bin = cms.vdouble(2.1, 2.4, 0.9,1.2 ,1.000)),
-    cms.PSet(bin = cms.vdouble(2.1, 2.4, 1.2,2.1 ,1.000)),
-    cms.PSet(bin = cms.vdouble(2.1, 2.4, 2.1,2.4 ,1.000)),
+singleEleTrigSF_Run2012= cms.VPSet(  ### eta bin of lepton, eta bin of neutrino, eff  ## bin neutrino is used for zz
+    cms.PSet(bin = cms.vdouble(0.0,   1.4442,   0.0,   9999.,     0.991)),
+    cms.PSet(bin = cms.vdouble(1.566,    2.5,   0.0,   9999.,     0.976)),
     
     )
 
