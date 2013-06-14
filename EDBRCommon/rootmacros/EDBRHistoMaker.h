87b133393fad3031818657cfb0c8328455bc3537
shuai <>
2013-06-14 10:52:59 +0000

    cout mZZ_type2_ptUncorrected

--- EDBRCommon/rootmacros/EDBRHistoMaker.h
+++ EDBRCommon/rootmacros/EDBRHistoMaker.h
@@ -1080,20 +1080,20 @@ void EDBRHistoMaker::Loop(std::string outFileName){
 					//-- END of ALL CUTS --
 
 					//Printout for debugging	
-					/*					       		       
-					if(mZZ_type2[ivec]>1800)
+										       		       
+					if(mZZ_type2_ptUncorrected[ivec]>1800)
 					  {
 					    //RunNumber:LumiSection:EvtNumber
 					    cout << run << ":" << ls << ":" << event << endl;
 					    
-					    cout << event             << " * " << run           << " * " << mZZ_type2[ivec] << " * " 
+					    cout << event             << " * " << run           << " * " <<mZZ_type2_ptUncorrected[ivec] << " * " 
 						 << ptlep1[ivec]      << " * " << etalep1[ivec] << " * " << philep1[ivec]   << " * "   
 						 << met               << " * " << philep2[ivec] << " * " << mt[ivec]        << " * " 
 						 << ptZll[ivec]                                                         << " * "
 					         << ptjet1[ivec]      << " * " << etajet1[ivec] << " * " << phijet1[ivec]   << " * " 
 					         << mJJNoKinFit[ivec] << " * " << nsubj21[ivec] << endl << endl; 					    
 					  }
-					  */
+					  
 					
 				       
 					
