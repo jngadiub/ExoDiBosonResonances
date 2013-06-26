fd13d143d989cb633b5a4f4adca22b9d6e46f6c9
bonato <>
2013-06-26 07:38:45 +0000

    Update list of cmgTuple for MC (production v2b)

--- EDBRCommon/python/datasets/cmgTuple_BulkG_ZZ_lljj_c0p2_M1600_cff.py
+++ EDBRCommon/python/datasets/cmgTuple_BulkG_ZZ_lljj_c0p2_M1600_cff.py
@@ -8,5 +8,5 @@ source = cms.Source("PoolSource",
                    )
 
 cmgFiles.extend([
-    '/store/cmst3/group/exovv/CMGtuple/productionv2a/Summer12/BulkG_ZZ_lljj_c0p2_M1600_v2/cmgTuple_0.root',
+    '/store/cmst3/group/exovv/CMGtuple/productionv2b/Summer12/BulkG_ZZ_lljj_c0p2_M1600/cmgTuple_0.root',
     ])
