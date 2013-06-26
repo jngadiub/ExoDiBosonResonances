87edd9f3bfd1ce8ffa00af3437645fdf6f9dd373
shuai <>
2013-06-26 12:38:47 +0000

    fisrt version of BTag weight

--- EDBRElectron/python/BTagWeights_Wele_cff.py
+++ EDBRElectron/python/BTagWeights_Wele_cff.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 BTagWeightsKinFitEle = cms.EDProducer("BTagWeightProducerWelenuDiJet",
                                     src = cms.InputTag("HLTWeightsKinFitEle") ,
+									BTagJets=cms.InputTag("jetAK5"),
+									EffmapFilename = cms.FileInPath("ExoDiBosonResonances/EDBRCommon/data/BtaggingEffs/WW_TuneZ2star_8TeV_pythia6_tauola_AK5PF_CSVM_bTaggingEfficiencyMap.root"),
+									scale = cms.double(0),#0 noscale, 1 scale up, -1 scale down
                                     )
 
     
@@ -11,7 +14,10 @@ BTagWeightsEle.src = "HLTWeightsEle"
     
     
 BTagWeightsMergedEle =cms.EDProducer("BTagWeightProducerWelenuSingleJet",
-                                   src = cms.InputTag("HLTWeightsMergedEle") ,
+                                   src = cms.InputTag("HLTWeightsMergedEle"),
+								   BTagJets=cms.InputTag("jetAK5"),
+								   EffmapFilename = cms.FileInPath("ExoDiBosonResonances/EDBRCommon/data/BtaggingEffs/WW_TuneZ2star_8TeV_pythia6_tauola_AK5PF_CSVM_bTaggingEfficiencyMap.root"),
+								   scale = cms.double(0),#0 noscale, 1 scale up, -1 scale down
                                    )
 
 
--- EDBRMuon/python/BTagWeights_Wmu_cff.py
+++ EDBRMuon/python/BTagWeights_Wmu_cff.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-
-
 BTagWeightsKinFitMu = cms.EDProducer("BTagWeightProducerWmunuDiJet",
                                     src = cms.InputTag("HLTWeightsKinFitMu") ,
+									BTagJets=cms.InputTag("jetAK5"),
+									EffmapFilename = cms.FileInPath("ExoDiBosonResonances/EDBRCommon/data/BtaggingEffs/WW_TuneZ2star_8TeV_pythia6_tauola_AK5PF_CSVM_bTaggingEfficiencyMap.root"),
+									scale = cms.double(0),#0 noscale, 1 scale up, -1 scale down
                                     )
 
     
@@ -14,6 +15,9 @@ BTagWeightsMu.src = "HLTWeightsMu"
     
 BTagWeightsMergedMu =cms.EDProducer("BTagWeightProducerWmunuSingleJet",
                                    src = cms.InputTag( "HLTWeightsMergedMu") ,
+								   BTagJets=cms.InputTag("jetAK5"),
+								   EffmapFilename = cms.FileInPath("ExoDiBosonResonances/EDBRCommon/data/BtaggingEffs/WW_TuneZ2star_8TeV_pythia6_tauola_AK5PF_CSVM_bTaggingEfficiencyMap.root"),
+								   scale = cms.double(0),#0 noscale, 1 scale up, -1 scale down
                                    )
 
 
