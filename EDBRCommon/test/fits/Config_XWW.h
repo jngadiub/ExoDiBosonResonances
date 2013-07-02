f13cf39697ad8f4fe77dab1c2fd61f8bca8d4113
shuai <>
2013-07-02 09:20:02 +0000

    v9 tree

--- EDBRCommon/test/fits/Config_XWW.h
+++ EDBRCommon/test/fits/Config_XWW.h
@@ -1,7 +1,7 @@
 
-const std::string myOutDir="FitSidebandsMJJ_CA8_WW_V21/";
-const string inDirSIG="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv8/AnaSigTree/";
-const string inDirSB ="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv8/AnaSBTree/";
+const std::string myOutDir="FitSidebandsMJJ_CA8_WW_V24/";
+const string inDirSIG="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv9/AnaSigTree/";
+const string inDirSB ="/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv9/AnaSBTree/";
 
 unsigned int jetCats = 1;//1 for only 1 jet case, 2 for both
 bool isZZChannel=false;//this will change only the file list
@@ -13,7 +13,7 @@ const unsigned int nToys = 500;
 const float lumi =19538.85;//for mu
 
 
-const bool doPseudoExp=false; //if true, for for different psuedo-alpha 
+const bool doPseudoExp=true; //if true, for for different psuedo-alpha 
 const bool unblind=true;//default is not to plot the data in signal region
 const bool decorrLevExpo=true;
 const bool plotDecorrLevExpoMain=true;//choose what to plot as main fit function
@@ -25,6 +25,6 @@ const bool alphaPoisson=false;//use Poisson errors for alpha uncertainties
 std::string bkgd_decorr_name="levexp_dcr_xww";
 
 
-const std::string datacardDir("DataCards_XWW_V21/");
+const std::string datacardDir("DataCards_XWW_V24/");
 float mZZmax_=3000;
 
