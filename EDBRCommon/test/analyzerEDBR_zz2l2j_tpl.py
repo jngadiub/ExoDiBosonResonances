ff7e0161f15247615512a3e6f5c1c1204fe943d7
shuai <>
2013-07-10 20:11:03 +0000

    add LP V tagging scale factor

--- EDBRCommon/test/analyzerEDBR_zz2l2j_tpl.py
+++ EDBRCommon/test/analyzerEDBR_zz2l2j_tpl.py
@@ -542,9 +542,11 @@ print '---> Ngen=',process.ANEDBR.Ngen,'  Xsect=',process.ANEDBR.xsec
 
 ###apply V-tagging scale factor only to signal MC samples
 if "BulkG_ZZ_lljj_" in "<SAMPLE>" :
-       process.ANEDBR.VTaggingScaleFactor=cms.double(0.95)
+       process.ANEDBR.VTaggingScaleFactorHP=cms.double(0.95)
+	   process.ANEDBR.VTaggingScaleFactorLP=cms.double(0.95)
 if "BulkG_WW_lvjj_" in "<SAMPLE>" or "RSG_WW_lvjj_" in "<SAMPLE>" or "WW_xww"=="<SAMPLE>" or "WZ_xww"=="<SAMPLE>" or "ZZ_xww"=="<SAMPLE>":
-       process.ANEDBR.VTaggingScaleFactor=cms.double(0.95)
+       process.ANEDBR.VTaggingScaleFactorHP=cms.double(0.93)
+	   process.ANEDBR.VTaggingScaleFactorLP=cms.double(1.1)
 
 process.filterFinalSelPath = cms.EDFilter("HLTHighLevel",
                                        TriggerResultsTag = cms.InputTag("TriggerResults","","CMG"),
