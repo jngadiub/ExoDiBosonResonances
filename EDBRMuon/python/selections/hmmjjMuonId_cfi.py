87aa3703c4ec715f37d4b1e07d3d02c6a9ae39f8
tomei <>
2013-07-17 12:42:00 +0000

    We added the new TuneP muons but we didn't change the selection... now this has been fixed.

--- EDBRMuon/python/selections/hmmjjMuonId_cfi.py
+++ EDBRMuon/python/selections/hmmjjMuonId_cfi.py
@@ -22,7 +22,8 @@ HPTmuonTKId2012 = cms.PSet(
     isTracker = cms.string('isTrackerMuon()'),
     #Updated for 2012 with new cut from the Muon POG
     #numberOfValidTrackerHits = cms.string('numberOfValidTrackerHits() > 10'),
-    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 8'),
+    #    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 8'),
+    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 5'),
     numberOfValidPixelHits = cms.string('numberOfValidPixelHits() > 0'),
     numberOfMatches = cms.string('numberOfMatchedStations() > 1'),
  #   normalizedChi2 = cms.string('normalizedChi2() < 10'),
@@ -36,7 +37,8 @@ HPTmuonGlobalId2012 = cms.PSet(
     isGlobal = cms.string('isGlobalMuon()'),
     #Updated for 2012 with new cut from the Muon POG
     #numberOfValidTrackerHits = cms.string('numberOfValidTrackerHits() > 10'),
-    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 8'),
+    #    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 8'),
+    trackerLayersWithMeasurement = cms.string('trackerLayersWithMeasurement() > 5'),
     numberOfValidPixelHits = cms.string('numberOfValidPixelHits() > 0'),
     numberOfValidMuonHits = cms.string('numberOfValidMuonHits() > 0'),
     numberOfMatches = cms.string('numberOfMatchedStations() > 1'),
