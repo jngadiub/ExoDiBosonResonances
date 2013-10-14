ad8ed1b9a7155c4201e96ffcc6bdef81db1b4143
Alessio Bonato <bonato@mail.cern.ch>
2013-10-14 12:52:43 +0200

    move channel option to the top of submit_Analyzer.sh

--- EDBRCommon/test/submit_AnalyzerEDBR.sh
+++ EDBRCommon/test/submit_AnalyzerEDBR.sh
@@ -3,7 +3,7 @@
 ### BulkG_ZZ_lljj_c0p2_M800 BulkG_ZZ_lljj_c0p2_M1100  BulkG_ZZ_lljj_c0p2_M1400 BulkG_ZZ_lljj_c0p2_M1800 
 
 TYPE="fullsig"  ### presel fullsb fullsig fullrange ttbar
-
+CHANNEL="ZZ" ###"WW" "ZZ"
 
 SampleName=( RSG_ZZ_lljj_c0p2_M1500 RSG_ZZ_lljj_c0p2_M1000 RSG_ZZ_lljj_c0p05_M1000  ) ###DoubleMu_Run2012A_22Jan2013 DoubleMuParked_Run2012B_22Jan2013 DoubleMuParked_Run2012C_22Jan2013 DoubleMuParked_Run2012D_22Jan2013 Photon_Run2012A_22Jan2013 DoublePhotonHighPt_Run2012B_22Jan2013 DoublePhotonHighPt_Run2012C_22Jan2013 DoublePhotonHighPt_Run2012D_22Jan2013  DYJetsPt70To100 DYJetsPt100 TTBARpowheg  DYJetsPt70To100 DYJetsPt100 WW WZ ZZ   BulkG_ZZ_lljj_c0p2_M600 BulkG_ZZ_lljj_c0p2_M700 BulkG_ZZ_lljj_c0p2_M800 BulkG_ZZ_lljj_c0p2_M900 BulkG_ZZ_lljj_c0p2_M1000 BulkG_ZZ_lljj_c0p2_M1100 BulkG_ZZ_lljj_c0p2_M1200 BulkG_ZZ_lljj_c0p2_M1300 BulkG_ZZ_lljj_c0p2_M1400 BulkG_ZZ_lljj_c0p2_M1500 BulkG_ZZ_lljj_c0p2_M1600 BulkG_ZZ_lljj_c0p2_M1700 BulkG_ZZ_lljj_c0p2_M1800 BulkG_ZZ_lljj_c0p2_M1900 BulkG_ZZ_lljj_c0p2_M2000 BulkG_ZZ_lljj_c0p2_M2100 BulkG_ZZ_lljj_c0p2_M2200 BulkG_ZZ_lljj_c0p2_M2300 BulkG_ZZ_lljj_c0p2_M2400 BulkG_ZZ_lljj_c0p2_M2500 ) 
 
@@ -25,7 +25,7 @@ SampleName=( RSG_ZZ_lljj_c0p2_M1500 RSG_ZZ_lljj_c0p2_M1000 RSG_ZZ_lljj_c0p05_M10
 for sample in  "${SampleName[@]}"
 do
 echo "Submitting $sample"
-bsub -q 8nh -J "treeEDBR_${sample}" run_AnalyzerEDBR.sh $sample "ZZ" $TYPE
+bsub -q 8nh -J "treeEDBR_${sample}" run_AnalyzerEDBR.sh $sample $CHANNEL $TYPE
 echo
 done
 
