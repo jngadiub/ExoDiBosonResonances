3cb0ea1466122a68fe24b2ebbc2877677f9c6e66
Shuai Liu <shuai@cern.ch>
2013-10-15 08:36:18 +0200

    update scripts

--- EDBRCommon/test/submit_split.sh
+++ EDBRCommon/test/submit_split.sh
@@ -4,8 +4,13 @@
 
 SampleName=( TTBARpowheg_xww WW_xww WZ_xww ZZ_xww WJetsPt100_xww WJetsPt180_xww DYJetsPt100_xww SingleTopBarSchannel_xww SingleTopBarTWchannel_xww SingleTopBarTchannel_xww SingleTopSchannel_xww SingleTopTWchannel_xww SingleTopTchannel_xww BulkG_WW_inclusive_c0p2_M600_xww BulkG_WW_inclusive_c0p2_M700_xww BulkG_WW_inclusive_c0p2_M800_xww BulkG_WW_inclusive_c0p2_M900_xww BulkG_WW_inclusive_c0p2_M1000_xww BulkG_WW_inclusive_c0p2_M1100_xww BulkG_WW_inclusive_c0p2_M1200_xww BulkG_WW_inclusive_c0p2_M1300_xww BulkG_WW_inclusive_c0p2_M1400_xww BulkG_WW_inclusive_c0p2_M1500_xww BulkG_WW_inclusive_c0p2_M1600_xww BulkG_WW_inclusive_c0p2_M1700_xww BulkG_WW_inclusive_c0p2_M1800_xww BulkG_WW_inclusive_c0p2_M1900_xww BulkG_WW_inclusive_c0p2_M2000_xww BulkG_WW_inclusive_c0p2_M2100_xww BulkG_WW_inclusive_c0p2_M2200_xww BulkG_WW_inclusive_c0p2_M2300_xww BulkG_WW_inclusive_c0p2_M2400_xww BulkG_WW_inclusive_c0p2_M2500_xww BulkG_WW_inclusive_M1000_W50_xww BulkG_WW_inclusive_M1000_W150_xww BulkG_WW_inclusive_M1000_W300_xww BulkG_WW_inclusive_M1500_W75_xww BulkG_WW_inclusive_M1500_W225_xww BulkG_WW_inclusive_M1500_W450_xww BulkG_WW_inclusive_M2100_W105_xww BulkG_WW_inclusive_M2100_W315_xww BulkG_WW_inclusive_M2100_W630_xww SingleMu_Run2012A-22Jan2013_xww SingleMu_Run2012B-22Jan2013_xww SingleMu_Run2012C-22Jan2013_xww SingleMu_Run2012D-22Jan2013_xww SingleElectron_Run2012A-22Jan2013_xww SingleElectron_Run2012B-22Jan2013_xww  SingleElectron_Run2012C-22Jan2013_xww SingleElectron_Run2012D-22Jan2013_xww )
 
-type="fullallrange" #### 'type' could be fullsideband or fullsig or presel or fullallrange, denpending on analyzerEDBR_zz2l2j_tpl.py
-OUTDIR=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv11/$type/
+PROCESS=fullrange              ###processFullSel=="fullsb" or processFullSel=="fullsig" or processFullSel=="fullrange" or processFullSel=="ttbarcontrol" 
+
+ANALYSIS=WW
+
+OUTDIR=/afs/cern.ch/work/s/shuai/public/diboson/trees/productionv12/$PROCESS/
+
+
 
 QUEUE=2nd
 
@@ -34,10 +39,10 @@ do
 
 	#create the cfg in the log dir of the sample
 	cd $OUTDIR/logs/$sample
-	sed -e "s/<SAMPLE>/${sample}/g" < ${MYWORKAREA}/ExoDiBosonResonances/EDBRCommon/test/analyzerEDBR_zz2l2j_tpl.py >  analyzerEDBR_${type}_${sample}_cfg.py
+	cat ${MYWORKAREA}/ExoDiBosonResonances/EDBRCommon/test/analyzerEDBR_zz2l2j_tpl.py  | sed -e s%"<SAMPLE>"%${sample}%g | sed -e s%"<ANALYSIS>"%${ANALYSIS}%g | sed -e s%"<PROCESS>"%${PROCESS}%g  > analyzerEDBR_${PROCESS}_${sample}_cfg.py
 	echo Config file created
 
 	#submit the job there, using cmsBatch
-	${MYWORKAREA}/ExoDiBosonResonances/EDBRCommon/test/cmsBatch.py $NFILES analyzerEDBR_${type}_${sample}_cfg.py  -b "bsub -q "$QUEUE" -J "treeEDBR_${type}_${sample}" < batchScript.sh"
+#	${MYWORKAREA}/ExoDiBosonResonances/EDBRCommon/test/cmsBatch.py $NFILES analyzerEDBR_${PROCESS}_${sample}_cfg.py  -b "bsub -q "$QUEUE" -J "treeEDBR_${PROCESS}_${sample}" < batchScript.sh"
 done
 
