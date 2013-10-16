2cc6f8a9631c0e69806c017aa3cc5e9ca7cd8e0f
Alessio Bonato <bonato@mail.cern.ch>
2013-10-16 10:32:59 +0200

    Add lists of PAT-tuples for new signal samples: Wprime and RS1 Grav (Pythia6)

---
+++ EDBRCommon/python/datasets/summer12_WprimeWZ_inclusive_M2500_PYTHIA6_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ('PoolSource',fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend([
+       '/store/cmst3/group/exovv/bonato/EDBR_PATtuple_edbr_zz_Summer12MC_WprimeWZ_PYTHIA_20131013_190656/bonato/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6/EDBR_PATtuple_edbr_zz_20131013_Summer12MC_WprimeWZ_PYTHIA/1b325ddfb984c14533be7920e22baeef/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6__Summer12_DR53X-PU_S10_START53_V19-v1__AODSIM_1_1_1Wt.root',
+       '/store/cmst3/group/exovv/bonato/EDBR_PATtuple_edbr_zz_Summer12MC_WprimeWZ_PYTHIA_20131013_190656/bonato/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6/EDBR_PATtuple_edbr_zz_20131013_Summer12MC_WprimeWZ_PYTHIA/1b325ddfb984c14533be7920e22baeef/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6__Summer12_DR53X-PU_S10_START53_V19-v1__AODSIM_2_1_nzY.root',
+       '/store/cmst3/group/exovv/bonato/EDBR_PATtuple_edbr_zz_Summer12MC_WprimeWZ_PYTHIA_20131013_190656/bonato/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6/EDBR_PATtuple_edbr_zz_20131013_Summer12MC_WprimeWZ_PYTHIA/1b325ddfb984c14533be7920e22baeef/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6__Summer12_DR53X-PU_S10_START53_V19-v1__AODSIM_3_1_aJr.root',
+       '/store/cmst3/group/exovv/bonato/EDBR_PATtuple_edbr_zz_Summer12MC_WprimeWZ_PYTHIA_20131013_190656/bonato/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6/EDBR_PATtuple_edbr_zz_20131013_Summer12MC_WprimeWZ_PYTHIA/1b325ddfb984c14533be7920e22baeef/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6__Summer12_DR53X-PU_S10_START53_V19-v1__AODSIM_4_1_KCg.root',
+       '/store/cmst3/group/exovv/bonato/EDBR_PATtuple_edbr_zz_Summer12MC_WprimeWZ_PYTHIA_20131013_190656/bonato/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6/EDBR_PATtuple_edbr_zz_20131013_Summer12MC_WprimeWZ_PYTHIA/1b325ddfb984c14533be7920e22baeef/WprimeToWZ_M_2500_TuneZ2star_8TeV_pythia6__Summer12_DR53X-PU_S10_START53_V19-v1__AODSIM_5_1_9j4.root'
+]);
