6c6fd3e9ac10d5b845d2b0737ce2ce74fd45723f
Thiago <Thiago.Tomei@cern.ch>
2013-10-22 22:14:16 +0200

    Tailoring the machinery to be more agnostic in between ZZ/WW/hadronic

--- EDBRCommon/test/fits/scripts_combine/gridificateCombine.sh
+++ EDBRCommon/test/fits/scripts_combine/gridificateCombine.sh
@@ -2,18 +2,43 @@
 
 if [ $# -lt 1 ]
 then
-    echo "Usage: ./gridificateCombine.sh <mass> [width]"
+    echo "Usage: ./gridificateCombine.sh <mass>"
     exit 1
 fi
 
+########## CHANGES GO HERE ##########
+card="comb_xzz.txt"
+channel="ZZ"
+# To change number of toys and iterations, change run_fullCLs_TF.py
+######### END CHANGES ##########
+
+### Information
 mass=$1
+echo "Mass is $mass,   card is $card,   channel is $channel"
+
 subdir=$mass
 
 # copy the relevant files inside the mass subdirs
+echo "Copying files inside subdir $subdir"
 cp crab.cfg run_fullCLs_TF.py pset.py ${subdir}
-sed "s/sedMP/${mass}/g" run_fullCLs_TF.sh > $subdir/run_fullCLs_TF.sh
+
+# go inside the subdir and quickly run combine (asymptotic) 
+# to get a handle of what the limit should be. In this way
+# we avoid the big array of limits
+echo "Running asymptotic limit to get a hint of where the limit should lie"
+cd ${subdir}
+ASYMPTOTICLIMIT=`combine ${card} -M Asymptotic 2> /dev/null | grep 'Observed Limit' | awk '{print $5}'`
+cd -
+
+echo "Preparing executable script for CRAB"
+sed "s/sedMP/${mass}/g" run_fullCLs_TF.sh |\
+sed "s/sedasympt/${ASYMPTOTICLIMIT}/g" |\
+sed "s/sedcard/${card}/g"|\
+sed "s/sedchannel/${channel}/g">\
+$subdir/run_fullCLs_TF.sh
 
 # go in the mass subdir, create and submit the job
+echo "Submitting CRAB jobs"
 cd ${subdir}
 crab -create
 crab -submit
--- EDBRCommon/test/fits/scripts_combine/run_fullCLs_TF.sh
+++ EDBRCommon/test/fits/scripts_combine/run_fullCLs_TF.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+RARRAYPOS=`expr $1 - 1`
 MASSPOINT=sedMP
-RVALUE=`expr $1 - 1`
-echo Argument 1 is ${RVALUE} # CRAB jobs start from 1, python arrays start from 0
+ASYMPTOTICVALUE=sedasympt
+CARDNAME=sedcard
+CHANNEL=sedchannel
+
+echo Argument 1 is ${RARRAYPOS} # CRAB jobs start from 1, python arrays start from 0
 echo Argument 2 is ${MASSPOINT}
-python run_fullCLs_TF.py ${RVALUE} ${MASSPOINT}
+python run_fullCLs_TF.py ${RARRAYPOS} ${MASSPOINT} ${ASYMPTOTICVALUE} ${CARDNAME} ${CHANNEL}
 echo; echo '----';
 ls -lh;
 echo '----';
