a56ffc5adbedaf4f2b332cfc04c316ee145a9d20
Shuai Liu <shuai@cern.ch>
2013-10-30 12:28:33 +0100

    add cmgtuple list for wprime and rsgww

---
+++ EDBRCommon/python/datasets/cmgTupleList_XWW/cmgTuple_1012/cmgTuple_RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+cmgFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",
+                    noEventSort = cms.untracked.bool(True),
+                    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                    fileNames = cmgFiles
+                   )
+
+cmgFiles.extend([
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_0.root',
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_1.root',
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_2.root',
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_3.root',
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_4.root',
+    '/store/cmst3/group/exovv/CMGtuple/shuai/production1011/Summer12/CA8//RSG_WW_lvjj_c0p05_M1500_PYTHIA6_xww/cmgTuple_5.root',
+    ])
---
+++ EDBRCommon/python/datasets/cmgTuple_WprimeWZ_inclusive_M1800_PYTHIA6_xww_cff.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+cmgFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",
+                    noEventSort = cms.untracked.bool(True),
+                    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                    fileNames = cmgFiles
+                   )
+
+cmgFiles.extend([
+    ])
